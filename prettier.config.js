a8c4b57b5c8407e744da3f66de7ceb32c9267e61
Valter Oliveira <vrauusx@gmail.com>
2025-03-21 20:24:16 -0300

    chore: add default Prettier configuration to format code with consistency

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+**/
+
+const config = {
+  trailingComma: "none",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  endOfLine: "auto"
+};
+
+export default config;
