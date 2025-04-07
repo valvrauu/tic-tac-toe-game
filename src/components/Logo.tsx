3b7e3722af26699a05f97efd670395843a542e0c
Valter Oliveira <vrauusx@gmail.com>
2025-04-07 20:16:53 -0300

    refactor(components/Logo): replace width/height utilities with size utility class

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,8 +4,8 @@ import IconO from "../assets/icons/icon-o.svg?react";
 function Logo() {
   return (
     <div className="flex w-max gap-2">
-      <IconX className="h-8 w-8" />
-      <IconO className="h-8 w-8" />
+      <IconX className="size-8" />
+      <IconO className="size-8" />
     </div>
   );
 }
