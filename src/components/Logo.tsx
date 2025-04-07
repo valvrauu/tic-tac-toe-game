3f07dc311ca6bc54dfe6a8cf8155a103bee20c76
Valter Oliveira <vrauusx@gmail.com>
2025-04-07 20:20:41 -0300

    feat(components/Logo): add fill colors to SVGs X and O components

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,8 +4,8 @@ import IconO from "../assets/icons/icon-o.svg?react";
 function Logo() {
   return (
     <div className="flex w-max gap-2">
-      <IconX className="size-8" />
-      <IconO className="size-8" />
+      <IconX className="fill-turquoise size-8" />
+      <IconO className="fill-saffron size-8" />
     </div>
   );
 }
