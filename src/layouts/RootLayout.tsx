a45a73a64eec464ffe954b63efe82b0bd746cd00
Valter Oliveira <vrauusx@gmail.com>
2025-04-14 23:47:10 -0300

    feat(layouts/RootLayout): center main content in Menu component based on pathname logic

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,10 +1,15 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import { GameContextProvider } from "../contexts/GameContext";
 
 function RootLayout() {
+  const location = useLocation();
+  const isMenu = location.pathname === "/";
+
   return (
     <GameContextProvider>
-      <Outlet />
+      <main className={isMenu ? "grid flex-1 place-content-center" : ""}>
+        <Outlet />
+      </main>
     </GameContextProvider>
   );
 }
