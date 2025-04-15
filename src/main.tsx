a3b582d624dced30e29ab5ce9b1781db5f035cb1
Valter Oliveira <vrauusx@gmail.com>
2025-04-14 23:51:07 -0300

    refactor(routes): create separate routes file for Menu and Game, and import into main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,38 +1,7 @@
-import { createBrowserRouter, RouterProvider } from "react-router";
 import { createRoot } from "react-dom/client";
-
-import Layout from "./layouts/Layout";
-import RootLayout from "./layouts/RootLayout";
-
-import Home from "./pages/Home";
-import Game from "./pages/Game";
-
+import { RouterProvider } from "react-router";
+import { router } from "./routes";
 import "./index.css";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <RootLayout />,
-    children: [
-      {
-        index: true,
-        element: (
-          <Layout position="center">
-            <Home />
-          </Layout>
-        )
-      },
-      {
-        path: "game",
-        element: (
-          <Layout position="top">
-            <Game />
-          </Layout>
-        )
-      }
-    ]
-  }
-]);
 
 const root = createRoot(document.getElementById("root")!);
 root.render(<RouterProvider router={router} />);
