f845e19b5d1f7f1fc5c59989e6a4dac3a0f2d27d
Valter Oliveira <vrauusx@gmail.com>
2025-04-18 09:10:31 -0300

    refactor(contexts/GameContext): change values to lowercase (x | o, cpu | player)

--- src/contexts/GameContext/GameContextProvider.tsx
+++ src/contexts/GameContext/GameContextProvider.tsx
@@ -2,8 +2,8 @@ import { useState, ReactNode } from "react";
 import { GameContext } from "./GameContext";
 
 export function GameContextProvider({ children }: { children: ReactNode }) {
-  const [playerMark, setPlayerMark] = useState<"X" | "O">("X");
-  const [mode, setMode] = useState<"CPU" | "Player">("CPU");
+  const [playerMark, setPlayerMark] = useState<"x" | "o">("x");
+  const [mode, setMode] = useState<"cpu" | "player">("cpu");
 
   return (
     <GameContext.Provider value={{ playerMark, mode, setPlayerMark, setMode }}>
