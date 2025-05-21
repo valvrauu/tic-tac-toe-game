989af61c29c96139a0f5e7559d5b42690b43cfce
Valter Oliveira <vrauusx@gmail.com>
2025-05-21 19:10:29 -0300

    feat(utils/game): add minimax algorithm for CPU move evaluation

--- src/utils/game/index.ts
+++ src/utils/game/index.ts
@@ -2,4 +2,5 @@ export * from "./checkMovesLeft";
 export * from "./checkWinner";
 export * from "./getScores";
 export * from "./getStatus";
+export * from "./minimax";
 export * from "./resetBoard";
