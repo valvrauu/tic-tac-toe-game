d33231b5cae8b7f4ff9dda34e90626c47fccb104
Valter Oliveira <vrauusx@gmail.com>
2025-05-21 20:39:33 -0300

    refactor(imports): replace relative imports with alias @/
    Update paths in routes.tsx and main.tsx to use @/... instead of ./

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import router from "@/routes";
-import "./index.css";
+import "@/index.css";
 
 const root = createRoot(document.getElementById("root")!);
 root.render(<RouterProvider router={router} />);
