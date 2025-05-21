6775ce442cad10d4ba72c45ddeaafd95fdadf43d
Valter Oliveira <vrauusx@gmail.com>
2025-05-21 19:11:47 -0300

    feat(utils/game): add makeMove helper to apply player actions to board

--- src/utils/game/index.ts
+++ src/utils/game/index.ts
@@ -3,5 +3,6 @@ export * from "./checkWinner";
 export * from "./findBestMove";
 export * from "./getScores";
 export * from "./getStatus";
+export * from "./makeMove";
 export * from "./minimax";
 export * from "./resetBoard";
