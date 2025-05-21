cd8a90d94a6ec607ed991bfabf9f4fd43bdafe4e
Valter Oliveira <vrauusx@gmail.com>
2025-05-21 19:10:54 -0300

    feat(utils/game): implement findBestMove to determine optimal CPU play

--- src/utils/game/index.ts
+++ src/utils/game/index.ts
@@ -1,5 +1,6 @@
 export * from "./checkMovesLeft";
 export * from "./checkWinner";
+export * from "./findBestMove";
 export * from "./getScores";
 export * from "./getStatus";
 export * from "./minimax";
