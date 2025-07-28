91b71a221c455f36d12bd715477be2048e804e79
Valter Oliveira <vrauusx@gmail.com>
2025-07-28 10:46:54 -0300

    style(utils/checkWinner): move prettier-ignore to top of function for organization

--- src/utils/game/checkWinner.ts
+++ src/utils/game/checkWinner.ts
@@ -9,8 +9,8 @@ type WinnerResult = {
   lineType: "row" | "column" | "diagonal";
 };
 
+// prettier-ignore
 function checkWinner(board: Board): WinnerResult | null {
-  // prettier-ignore
   const winningPatterns: {
     positions: WinningLine;
     type: "row" | "column" | "diagonal";
