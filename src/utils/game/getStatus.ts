47841d6595ca6855ae3a0d37a82752f367b29623
Valter Oliveira <vrauusx@gmail.com>
2025-07-29 11:45:27 -0300

    refactor(utils/getStatus/v001): restructure and simplify logic
    - Define GetStatusResult type for more precise return typing.
    - Improve conditionals for winner, draw, and progress states.

--- src/utils/game/getStatus.ts
+++ src/utils/game/getStatus.ts
@@ -1,40 +1,73 @@
 import { Mode, Players, Board } from "@/types";
-import { checkWinner, checkMovesLeft } from "@/utils/game";
+import { checkWinner, WinnerResult } from "@/utils/game";
+import { checkMovesLeft } from "@/utils/game";
 
-type getStatusProps = {
+type GetStatusProps = {
   mode: Mode;
   board: Board;
   players: Players;
 };
 
-function getStatus({ mode, board, players }: getStatusProps) {
+type GetStatusResult =
+  | {
+      status: "win";
+      roundOutcomeTitle: string;
+      roundOutcomeMessage: string;
+      winnerResult: WinnerResult;
+    }
+  | {
+      status: "draw";
+      roundOutcomeTitle: string;
+      roundOutcomeMessage: string | null;
+      winnerResult: null;
+    }
+  | {
+      status: "progress";
+      roundOutcomeTitle: null;
+      roundOutcomeMessage: null;
+      winnerResult: null;
+    };
+
+function getStatus({ mode, board, players }: GetStatusProps): GetStatusResult {
   const isSinglePlayer = mode === "singleplayer";
+  const winnerResult = checkWinner(board);
 
-  const hasWinner = checkWinner(board);
-  const isDraw = !hasWinner && !checkMovesLeft(board);
-  const isActive = !hasWinner && !isDraw;
+  if (winnerResult) {
+    const isPlayerWinner = winnerResult.winner === players.player;
 
-  const title = hasWinner ? "Takes the Round" : "Round Tied";
-
-  const subTitle = hasWinner
-    ? isSinglePlayer
-      ? hasWinner.winner === players.player
+    const roundOutcomeTitle = "Takes the Round";
+    const roundOutcomeMessage = isSinglePlayer
+      ? isPlayerWinner
         ? "You Won!"
         : "Oh No, You Lost…"
-      : hasWinner.winner === players.player
+      : isPlayerWinner
         ? "Player 1 Wins"
-        : "Player 2 Wins"
-    : "It's a draw!";
+        : "Player 2 Wins";
 
-  const icon = hasWinner ? hasWinner.winner : null;
+    return {
+      status: "win",
+      roundOutcomeTitle,
+      roundOutcomeMessage,
+      winnerResult
+    };
+  }
+
+  const isDraw = !checkMovesLeft(board);
+
+  if (isDraw) {
+    return {
+      status: "draw",
+      roundOutcomeTitle: "Round Tied",
+      roundOutcomeMessage: null,
+      winnerResult: null
+    };
+  }
 
   return {
-    title,
-    subTitle,
-    icon,
-    hasWinner,
-    isDraw,
-    isActive
+    status: "progress",
+    roundOutcomeTitle: null,
+    roundOutcomeMessage: null,
+    winnerResult: null
   };
 }
 
