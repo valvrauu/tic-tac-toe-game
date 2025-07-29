116453a3cc5ddded76c878d0b5e0f40e9752986c
Valter Oliveira <vrauusx@gmail.com>
2025-07-29 11:57:35 -0300

    refactor(components/Modal): rename getIconAndColor and improve conditional rendering
    - Rename getIconAndColor to getIconData and update its properties.
    - Use && operator to conditionally render title and subtitle in JSX.

--- src/components/Game/Modal.tsx
+++ src/components/Game/Modal.tsx
@@ -15,33 +15,33 @@ type ModalLinkProps = {
 } & ModalButtonProps;
 
 type ModalProps = {
-  title: string;
-  subTitle: string;
-  icon: Cell;
+  title: string | null;
+  subTitle: string | null;
+  icon: Cell | null;
   onCancel: ModalLinkProps;
   onAccept: ModalButtonProps;
   className?: string;
 };
 
-function getIconAndColor(icon: Cell): { icon: ReactElement; color: string } {
+function getIconData(icon: Cell): { element: ReactElement; color: string } {
   switch (icon) {
     case "x":
       return {
-        icon: (
+        element: (
           <IconX className="fill-turquoise stroke-turquoise tablet:size-16 size-7" />
         ),
         color: "text-turquoise"
       };
     case "o":
       return {
-        icon: (
+        element: (
           <IconO className="fill-saffron stroke-saffron tablet:size-16 size-7.5" />
         ),
         color: "text-saffron"
       };
     default:
       return {
-        icon: <></>,
+        element: <></>,
         color: "text-casper"
       };
   }
@@ -55,7 +55,7 @@ function Modal({
   onAccept,
   className
 }: ModalProps) {
-  const { icon: Icon, color: titleColor } = getIconAndColor(icon);
+  const { element, color } = getIconData(icon);
 
   return (
     <div className="absolute inset-0 flex min-w-75 items-center bg-black/50">
@@ -66,19 +66,23 @@ function Modal({
         )}
       >
         <div className="flex flex-col-reverse gap-4 font-bold uppercase">
-          <h1
-            className={cn(
-              "tablet:gap-6 tablet:text-3xl flex items-center gap-2 text-2xl tracking-wider",
-              titleColor
-            )}
-          >
-            {Icon}
-            {title}
-          </h1>
+          {title && (
+            <h1
+              className={cn(
+                "tablet:gap-6 tablet:text-3xl flex items-center gap-2 text-2xl tracking-wider",
+                color
+              )}
+            >
+              {element}
+              {title}
+            </h1>
+          )}
 
-          <h2 className="tablet:text-lg text-center tracking-wider">
-            {subTitle}
-          </h2>
+          {subTitle && (
+            <h2 className="tablet:text-lg text-center tracking-wider">
+              {subTitle}
+            </h2>
+          )}
         </div>
 
         <div className="flex gap-4">
