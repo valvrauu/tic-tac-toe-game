8ddad13b6bad06f14ed9ca0ec5ff4e3a07186311
Valter Oliveira <vrauusx@gmail.com>
2025-07-30 12:23:47 -0300

    refactor(utils/makeMove): return status and reason for invalid moves, board for valid ones

--- src/utils/game/makeMove.ts
+++ src/utils/game/makeMove.ts
@@ -1,18 +1,34 @@
 import { Symbol, Board } from "@/types";
 import { checkMovesLeft, checkWinner } from "@/utils/game";
 
-function makeMove(board: Board, row: number, col: number, playerMark: Symbol) {
+type MakeMoveResult =
+  | { status: "invalid"; reason: "game_over" | "cell_occupied" }
+  | { status: "valid"; board: Board };
+
+function makeMove(
+  board: Board,
+  row: number,
+  col: number,
+  playerMark: Symbol
+): MakeMoveResult {
   const hasWinner = checkWinner(board);
   const isDraw = !hasWinner && !checkMovesLeft(board);
 
-  if (hasWinner || isDraw) return;
-  if (board[row][col]) return;
+  if (hasWinner || isDraw) {
+    return { status: "invalid", reason: "game_over" };
+  }
 
-  return board.map((rowData, rowIndex) =>
+  if (board[row][col]) {
+    return { status: "invalid", reason: "cell_occupied" };
+  }
+
+  const updatedBoard = board.map((rowData, rowIndex) =>
     rowData.map((columnData, columnIndex) =>
       row === rowIndex && col === columnIndex ? playerMark : columnData
     )
   ) as Board;
+
+  return { status: "valid", board: updatedBoard };
 }
 
 export { makeMove };
